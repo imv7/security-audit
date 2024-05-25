eaeb8b3162e2e793c4768f3dbebe8a0c7a9d4ac9
imv <igormonteirovieira@gmail.com>
2024-05-25 06:31:12 -0300

    Update decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -1,7 +1,6 @@
 # Replace "/path/to/encrypted_folder.tar.gz.gpg" with the path to the encrypted folder
 # Replace "encryption_key.txt" with the path to the file containing the encryption key
 # Replace "/path/to/output/folder" with the desired path for the decrypted folder
-# Kyndryl Confidential
 
 # Decrypt the folder using the encryption key
 gpg --decrypt --output /path/to/output/folder/encrypted_folder.tar.gz /path/to/encrypted_folder.tar.gz.gpg
